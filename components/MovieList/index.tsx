d2168cb4f470d88322e41ab4118a2805e1896d82
Matt Sacks <github@mattsacks.email>
2020-02-06 09:00:51 -0800

    Add MovieList component to display a list of movies

---
+++ components/MovieList/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import { Movie } from "lib/types";
+import MovieItem from "components/MovieItem";
+
+interface Props {
+  label?: string;
+  movies: Movie[];
+}
+
+export default function MovieList({ label, movies }: Props) {
+  const MovieItems = movies.map((movie) => (
+    <Link key={movie.id} href="/movies/[movieId]" as={`/movies/${movie.id}`}>
+      <a className="hover:underline">
+        <MovieItem movie={movie} />
+      </a>
+    </Link>
+  ));
+
+  return (
+    <div className="my-6">
+      <div className="font-bold text-xl">{label}</div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+        {MovieItems}
+      </div>
+    </div>
+  );
+}
--- components/PopularMovies/index.tsx
+++ components/PopularMovies/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Link from "next/link";
 import useSWR from "swr";
 import { Movie } from "lib/types";
-import MovieItem from "components/MovieItem";
+import MovieList from "components/MovieList";
 
 export default function PopularMovies() {
   const { data, error } = useSWR("/movie/popular");
@@ -16,23 +15,9 @@ export default function PopularMovies() {
   }
 
   // Get the top 10 movies and remove any adult ones
-  const PopularMovies = data.results
+  const popularMovies = data.results
     .slice(0, 12)
-    .filter((movie: Movie) => !movie.adult)
-    .map((movie: Movie) => (
-      <Link key={movie.id} href="/movies/[movieId]" as={`/movies/${movie.id}`}>
-        <a className="hover:underline">
-          <MovieItem movie={movie} />
-        </a>
-      </Link>
-    ));
+    .filter((movie: Movie) => !movie.adult);
 
-  return (
-    <div className="my-6">
-      <div className="font-bold text-xl">Currently trending</div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-        {PopularMovies}
-      </div>
-    </div>
-  );
+  return <MovieList label="Currently trending" movies={popularMovies} />;
 }
