9d2f27695ab13d340ff3df69a076948de8fb6f4d
Matt Sacks <github@mattsacks.email>
2020-02-06 09:02:09 -0800

    Add query param handling for the homepage

--- components/MovieSearch/index.tsx
+++ components/MovieSearch/index.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import Router from "next/router";
+import { debounce } from "lodash";
 
-export default function MovieSearch() {
+interface Props {
+  query: string;
+  setQuery: (query: string) => void;
+}
+
+export default function MovieSearch({ query, setQuery }: Props) {
+  const [inputValue, setInputValue] = useState(query);
+
+  // If the input value doesn't match the current query, update the input value
+  useEffect(() => {
+    if (query !== inputValue) {
+      setInputValue(query);
+    }
+  }, [query]);
+
+  const debouncedSetQuery = useCallback(
+    debounce((newQuery: string) => {
+      setQuery(newQuery), 500;
+      Router.replace(newQuery ? `/?query=${newQuery}` : "/");
+    }, 500),
+    []
+  );
+
   return (
     <div className="flex flex-col text-xl w-full mb-8">
       <label className="block font-bold" htmlFor="movie-search">
         Find movies!
       </label>
       <input
-        className="appearance-none border-4 border-black rounded p-2 w-full"
+        className="appearance-none border-4 border-black p-2 w-full"
         id="movie-search"
         name="movie-search"
-        placeholder="Dumb & Dumber, Three Ninjas, Parasite"
-        type="text"
+        onChange={(e) => {
+          setInputValue(e.target.value);
+          debouncedSetQuery(e.target.value);
+        }}
+        placeholder="Mortal Kombat"
+        type="search"
+        value={inputValue}
       />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { useRouter } from "next/router";
 import Banner from "components/Banner";
 import MovieSearch from "components/MovieSearch";
 import SearchResults from "components/SearchResults";
 import PopularMovies from "components/PopularMovies";
 
 export default function Index() {
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+
+  // If the query param doesn't match what's being searched (ie, a page
+  // refresh), update the local query
+  if (router.query.query !== query) {
+    setQuery(router.query.query as string);
+  }
+
   return (
     <React.Fragment>
       <Banner />
